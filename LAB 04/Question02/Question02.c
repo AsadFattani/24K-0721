ac71188bcf9f5041471865db3a1eb217fd967abc
ASADFATTANI <asad.imran.fattani@gmail.com>
2024-09-10 20:40:16 +0500

    C language code for Question 1 and 2

--- LAB 04/Question02/Question02.c
+++ LAB 04/Question02/Question02.c
@@ -1,5 +1,39 @@
-<!--
-* Name: ASAD IMRAN
-* Date: 9 sept 24
-* Desc: Question01 pseudocode, and flowchart
--->
+#include <stdio.h>
+
+int main(){
+    char operator;
+    int num1, num2;
+
+    printf("Enter number 1: ");
+    scanf("%d", &num1);
+
+    getchar();
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+    
+    switch (operator){
+        case '+':
+            printf("The sum is %d\n ",num1 + num2);
+            break;
+        case '-':
+            printf("The subb is %d\n ",num1 - num2);
+            break;
+        case '*':
+            printf("The multi is %d\n ",num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("The division is %.2f\n", (float)num1 / num2);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+        default:
+            printf("Invalid operator.\n");
+            break;
+
+    }
+    return 0;
+}
--- LAB 04/Question02/Question02.md
+++ LAB 04/Question02/Question02.md
@@ -1,2 +1,6 @@
-
+<!--
+* Name: ASAD IMRAN
+* Date: 9 sept 24
+* Desc: Question01 pseudocode, and flowchart
+-->
 
